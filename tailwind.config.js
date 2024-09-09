c60b62ec88599c8895c0915a41cea76b1cd2e189
Pierre Cédric <cedric.pierre@scito.com>
2024-09-09 19:55:14 +0200

    Improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,6 @@ import colors from "tailwindcss/colors.js";
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
-        'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
