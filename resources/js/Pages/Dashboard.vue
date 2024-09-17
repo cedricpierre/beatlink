ec7e030401795eddde12e1d5c57bf58adb5937cd
Pierre Cédric <cedric.pierre@scito.com>
2024-09-17 12:58:25 +0200

    Fix

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -31,68 +31,67 @@ const columns = ref([
             <h2>Dashboard</h2>
         </template>
 
-        <div class="lg:max-w-screen-lg mx-auto">
-	        <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
-		        <div class="col-span-2">
-                    <Card>
-                        <template #header>Latest campains</template>
 
-                        <Table :columns="columns" :items="props.campaigns">
-                            <template #item.views_count="{item}">
-                                <Badge>{{ item.views_count }}</Badge>
-                            </template>
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
+            <div class="col-span-2">
+                <Card>
+                    <template #header>Latest campains</template>
 
-                            <template #item.leads_count="{item}">
-                                <Badge>{{ item.leads_count }}</Badge>
-                            </template>
-
-                            <template #item.conversion_rate="{item}">
-                                <Badge>{{ item.conversion_rate }}%</Badge>
-                            </template>
-                            <template #item.actions="{item}">
-                                <div class="space-x-2 whitespace-nowrap flex justify-end">
-	                                <a :href="route('landing', {slug: item.slug})" class="btn btn-neutral btn-sm" target="_blank">
-                                        Page
-                                    </a>
-	                                <Link :href="route('campaigns.view', {id: item.id})" class="btn btn-neutral btn-sm">
-                                        View
-                                    </Link>
-                                    <Link :href="route('campaigns.edit', {id: item.id})" class="btn btn-sm btn-primary">
-                                        Edit
-                                    </Link>
-                                </div>
-                            </template>
-                            <template #empty>
-                                <div class="flex space-y-6 flex-col justify-center items-center">
-                                    <span>No campaigns found</span>
-                                    <Link :href="route('campaigns.create')" class="btn btn-primary btn-sm btn-outline">Create a campaign now</Link>
-                                </div>
-                            </template>
-                        </Table>
-                    </Card>
-                </div>
-		        <div class="col-span-1">
-                    <Card>
-                        <template #header>
-                            Subscription
+                    <Table :columns="columns" :items="props.campaigns">
+                        <template #item.views_count="{item}">
+                            <Badge>{{ item.views_count }}</Badge>
                         </template>
 
-                        <template v-if="user.is_premium">
-                            <div>You are subscribed ! You can create up to {{ props.subscription?.quantity }} campaigns. If you want more, you should consider
-                                upgrading your plan.
-                            </div>
-                            <Link :href="route('subscriptions.edit')" class="btn btn-primary btn-sm">Upgrade</Link>
+                        <template #item.leads_count="{item}">
+                            <Badge>{{ item.leads_count }}</Badge>
                         </template>
-                        <div v-else class="space-y-6">
-                            <div>
-                                <p>Your subscription is inactive.</p>
-                                <p>You need to subscribe to reactivate your campaigns.</p>
+
+                        <template #item.conversion_rate="{item}">
+                            <Badge>{{ item.conversion_rate }}%</Badge>
+                        </template>
+                        <template #item.actions="{item}">
+                            <div class="space-x-2 whitespace-nowrap flex justify-end">
+                                <a :href="route('landing', {slug: item.slug})" class="btn btn-neutral btn-sm" target="_blank">
+                                    Page
+                                </a>
+                                <Link :href="route('campaigns.view', {id: item.id})" class="btn btn-neutral btn-sm">
+                                    View
+                                </Link>
+                                <Link :href="route('campaigns.edit', {id: item.id})" class="btn btn-sm btn-primary">
+                                    Edit
+                                </Link>
                             </div>
-                            <Link :href="route('subscriptions.subscribe')" class="btn btn-primary btn-sm">Subscribe</Link>
+                        </template>
+                        <template #empty>
+                            <div class="flex space-y-6 flex-col justify-center items-center">
+                                <span>No campaigns found</span>
+                                <Link :href="route('campaigns.create')" class="btn btn-primary btn-sm btn-outline">Create a campaign now</Link>
+                            </div>
+                        </template>
+                    </Table>
+                </Card>
+            </div>
+            <div class="col-span-1">
+                <Card>
+                    <template #header>
+                        Subscription
+                    </template>
+
+                    <template v-if="user.is_premium">
+                        <div>You are subscribed ! You can create up to {{ props.subscription?.quantity }} campaigns. If you want more, you should consider
+                            upgrading your plan.
                         </div>
+                        <Link :href="route('subscriptions.edit')" class="btn btn-primary btn-sm">Upgrade</Link>
+                    </template>
+                    <div v-else class="space-y-6">
+                        <div>
+                            <p>Your subscription is inactive.</p>
+                            <p>You need to subscribe to reactivate your campaigns.</p>
+                        </div>
+                        <Link :href="route('subscriptions.subscribe')" class="btn btn-primary btn-sm">Subscribe</Link>
+                    </div>
 
-                    </Card>
-                </div>
+                </Card>
             </div>
         </div>
     </AuthenticatedLayout>
