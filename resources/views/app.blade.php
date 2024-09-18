c009b04f7277e8064001be36f018db4d9bd9e144
Pierre Cédric <cedric.pierre@scito.com>
2024-09-18 23:31:34 +0200

    avoid scalable

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="{{$theme}}-{{isset($dark) && $dark ? 'dark' : 'light'}}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
     <title inertia>{{ config('app.name') }}</title>
