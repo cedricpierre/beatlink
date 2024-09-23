5f3a12f983e7fe1a23fd118d4389663fcd00e36c
Pierre Cédric <cedric.pierre@scito.com>
2024-09-23 14:30:40 +0200

    fuck it

--- resources/js/Pages/Error.vue
+++ resources/js/Pages/Error.vue
@@ -1,16 +1,15 @@
 <script lang="ts" setup>
 import {Head, Link} from '@inertiajs/vue3';
 import DefaultLayout from "@/Layouts/DefaultLayout.vue";
+import GuestLayout from "@/Layouts/GuestLayout.vue";
 </script>
 
 <template>
-    <Head title="Welcome"/>
-    <DefaultLayout>
-        <div class="container mx-auto lg:max-w-screen-lg">
+	<Head title="Error"/>
+	<GuestLayout>
 
-            <div class="text-lg py-8 ">Awww snap ! An error occured...</div>
-
+		<div class="container mx-auto lg:max-w-screen-lg">
+			<div class="text-lg py-8 text-center">Awww snap ! An error occured...</div>
         </div>
-
-    </DefaultLayout>
+	</GuestLayout>
 </template>
