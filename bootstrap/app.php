9497269c65035dc997074153d5ef5d05902ec30b
Pierre Cédric <cedric.pierre@scito.com>
2024-09-23 10:09:15 +0200

    debug

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -29,9 +29,9 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
 
-        $exceptions->respond(function (Response|RedirectResponse $response, Throwable $exception, Request $request) {
-            return $exception;
+        return $exceptions;
 
+        $exceptions->respond(function (Response|RedirectResponse $response, Throwable $exception, Request $request) {
             if (!app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
                 return Inertia::render('Error', ['status' => $response->getStatusCode()])
                               ->toResponse($request)
