75aeaba12b36766ab5116bfa2159530781fbdba4
Pierre Cédric <cedric.pierre@scito.com>
2024-09-23 13:05:54 +0200

    test

--- docker/8.3/start.sh
+++ docker/8.3/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo $APP_KEY
 echo "Starting services..."
-service php8.3-fpm start
+/usr/sbin php8.3-fpm -d variables_order=EGPCS
 nginx -g "daemon off;error_log /dev/stdout debug;"
 echo "Ready."
 tail -s 1 /var/log/nginx/*.log -f
