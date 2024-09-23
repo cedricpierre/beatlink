47936f3a7af8a661552d43b3a3cec1c4e055cd62
Pierre Cédric <cedric.pierre@scito.com>
2024-09-23 13:17:06 +0200

    fix

--- docker/8.3/start.sh
+++ docker/8.3/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo $APP_KEY
 echo "Starting services..."
-/usr/sbin php8.3-fpm -d variables_order=EGPCS
-nginx -g "daemon off;error_log /dev/stdout debug;"
+mkdir /run/php
+/usr/sbin/php-fpm8.3 -F -R -d variables_order=EGPCS
+nginx -g "daemon off"
 echo "Ready."
