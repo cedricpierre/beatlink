c2591c502956b79da7b4d8beb40e6464091226be
Pierre Cédric <cedric.pierre@scito.com>
2024-09-23 14:06:10 +0200

    fuck it

--- docker/8.3/start.sh
+++ docker/8.3/start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo "Starting services..."
-service php8.3-fpm start
-/usr/sbin/php-fpm8.3 -F -R -d variables_order=EGPCS
+service php8.3-fpm start -d variables_order=EGPCS
 nginx -g "daemon off"
 echo "Ready."
